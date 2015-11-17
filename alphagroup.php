3d2c0d8c2ef73259c2dbba27446274d7a57794da
Pamella Gaiguer Santos <pamella.santos@totvs.com.br>
2015-11-17 17:53:39 -0200

    convertendo site para php

---
+++ alphagroup.php
@@ -0,0 +1,7 @@
+    <?php include "_header.php";?>
+
+    <div>
+      <img src="images/interna_oalphagroup.jpg" height="2304" width="2405" alt="">
+
+    </div>
+    <?php include "_footer.php";?>
---
+++ index.php
@@ -0,0 +1,229 @@
+    <?php include "_header.php";?>
+
+    <section class="section div-home">
+     <div id="carousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <div class="fill" style="background-image:url('images/home_instituto_001.jpg');"></div>
+        </div>
+        <div class="item">
+          <div class="fill" style="background-image:url('images/home_instituto_002.jpg');"></div>
+        </div>
+        <div class="item">
+          <div class="fill" style="background-image:url('images/home_instituto_003.jpg');"></div>
+        </div>
+      </div>
+
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="arrow arrow-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="arrow arrow-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+  </section>
+
+  <section class="section fp-responsive div-instituto text-center">
+    <br>
+    <img src="images/bg2-divisor.png" height="86" width="670" alt="" class="center-block img-responsive">
+    <h2 class="green-text">
+      ​Bem vindo ao <strong>Alpha Group</strong><br>
+    </h2>
+    <p class="green-text">Juntos, praticamos a <strong>Medicina Integrativa Ambiental</strong>, em benefício dos nossos pacientes, pois vamos<br>
+      muito além do tratamento de doenças estabelecidas, oferecemos a possibilidade de preveni-las!</p>
+    <p>
+      É preciso buscar as verdadeiras raízes da doença. Quando o corpo exibe os sintomas, são <br>
+      somente as folhas de uma árvore.
+    </p>
+    <p>
+      Saúde se conquista diariamente, eliminando o que não serve mais ou o que é tóxico, <br>
+      e aportando novas informações de qualidade, nutrientes e pensamentos com igual importância.
+    </p>
+    <span class="text-center"><a class="link-box" href="alphagroup.php">Leia mais</a></span>
+  </section>
+
+  <section class="section fp-responsive div-especialidades text-center">
+    <div class="container">
+      <div class="row">
+        <h2><strong>Especialidades</strong></h2>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_001_medicina_integrativa.png" height="97" width="96" alt="medicina-integrativa">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Medicina Integrativa</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse eius quis, ipsum, ut repellat delectus perferendis nihil blanditiis, expedita molestias consectetur vero commodi. Quaerat iusto, neque, vero maxime eveniet praesentium.
+              </p>
+            </div>
+          </div>
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_002_odontologia_biologica.png" height="97" width="96" alt="odontologia-biologica">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Odontologia Biológica</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse eius quis, ipsum, ut repellat delectus perferendis nihil blanditiis, expedita molestias consectetur vero commodi. Quaerat iusto, neque, vero maxime eveniet praesentium.
+
+              </p>
+            </div>
+          </div>
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_003_terapias.png" height="97" width="96" alt="terapias">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Terapias</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi eum magnam, accusamus officia ea praesentium odio sapiente voluptate dolores error repellat vero sint tempora non, at adipisci quas quo nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_004_nutricao_funcional.png" height="97" width="96" alt="nutricao-funcional">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Nutrição Funcional</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quisquam necessitatibus impedit amet, voluptates, neque ab! Explicabo, voluptatibus eveniet quo laborum nemo, soluta illo, ipsa dicta officia quos mollitia labore.
+              </p>
+            </div>
+          </div>
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_005_estetica.png" height="97" width="96" alt="nutricao-funcional">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Estética</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, deserunt unde delectus eum sed eligendi accusantium magnam temporibus dolorem corrupti, amet, autem illum facere quisquam? Esse fugiat omnis adipisci ducimus.
+              </p>
+            </div>
+          </div>
+          <div class="media">
+            <div class="media-left media-top">
+              <a href="#">
+                <img class="media-object" src="images/espec_006_exames.png" height="97" width="96" alt="nutricao-funcional">
+              </a>
+            </div>
+            <div class="media-body">
+              <h4 class="media-heading text-left">Exames</h4>
+              <p class="text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, sed impedit quia. Accusantium, labore minima laborum dolorem animi! Maxime, enim sequi! Tenetur ab, sapiente reiciendis a nesciunt facilis exercitationem dolorem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section fp-responsive div-profissionais text-center">
+    <div class="container">
+      <h2><strong>Nossos profissionais</strong></h2>
+      <p>
+        Atendendo as necessidades da vida atual, o Instituto Alpha oferece uma estrutura completa de <br>
+        profissionais, equipamentos, diagnósticos e exames para a otimização do seu tempo e da sua visita.
+      </p>
+      <p>Conheça nossa equipe e o especialista de cada área nas fotos abaixo</p>
+    </div>
+    <div class="container">
+      <div id="slideshow">
+        <div>
+          <img src="images/profissionais/alexandre-almeida.png" height="383" width="313" alt="">
+          <img src="images/profissionais/amanda-mariano.png" height="383" width="313" alt="">
+          <img src="images/profissionais/lamia-ayoub.png" height="383" width="316" alt="">
+        </div>
+        <div>
+          <img src="images/profissionais/marcelo-marteleto.png" height="383" width="313" alt="">
+          <img src="images/profissionais/maria-emilia.png" height="383" width="313" alt="">
+          <img src="images/profissionais/roberta-safar.png" height="383" width="313" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section fp-responsive div-parceiros text-center">
+    <div class="container">
+      <h2>Parceiros de confiança</h2>
+      <p>
+        O Instituto Alpha de Saúde Integral, em função da competência de seus profissionais e serviços, estabeleceu parceria técnico-científica com a Paracelsus Klinik, a clínica mais antiga de Medicina Biológica do mundo, localizada em Lustmühle, Suiça.
+        <br>
+        Por meio desta associação, nossos pacientes dispõem de uma segunda opinião com o Dr. Thomas Rau, o mundialmente conhecido Diretor Médico da Paracelsus Klinik, bem como de tratamento personalizado com medicamentos importados que se façam necessários.
+      </p>
+    </div>
+    <div class="container bg-parceiros">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="partner">
+            <br>
+            <p class="center-block"><img src="images/logos_parceiros/logo1-paracelsus.jpg" height="70" width="282" alt="" class="img-responsive"></p>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <br><br>
+
+          </div>
+          <br>
+          <div class="partner">
+          <br>
+            <p class="center-block"><img src="images/logos_parceiros/logo3-institutomachado.jpg" class="img-responsive"></p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="partner">
+            <br>
+            <p class="center-block"><img src="images/logos_parceiros/logo2-katia.jpg" height="54" width="243" class="img-responsive"></p>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <br><br>
+            <br>
+          </div>
+          <br>
+          <div class="partner">
+            <p class="pull-right"><img src="images/logos_parceiros/logo4-victorsorrentino.jpg" height="120" class="img-responsive"></p>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+            <div class="clearfix"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include "_footer.php";?>
