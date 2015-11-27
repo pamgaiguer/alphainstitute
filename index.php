6da855439300af7727e9bda805f14278c6900c4c
Pamella Gaiguer Santos <pamella.santos@totvs.com.br>
2015-11-27 18:32:55 -0200

    melhorando css

--- index.php
+++ index.php
@@ -1,66 +1,75 @@
 <?php include "_header.php";?>
-		<!-- Slider -->
-		<section class="slide-inicial">
-    <div id="carousel" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
-      </ol>
+<!-- Slider -->
+<section class="slide-inicial">
+	<div id="carousel" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel" data-slide-to="1"></li>
+			<li data-target="#carousel" data-slide-to="2"></li>
+		</ol>
 
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <div class="fill" style="background-image:url('images/home_instituto_001.jpg');"></div>
-        </div>
-        <div class="item">
-          <div class="fill" style="background-image:url('images/home_instituto_002.jpg');"></div>
-        </div>
-        <div class="item">
-          <div class="fill" style="background-image:url('images/home_instituto_003.jpg');"></div>
-        </div>
-      </div>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<div class="fill" style="background-image:url('images/home_instituto_001.jpg');"></div>
+			</div>
+			<div class="item">
+				<div class="fill" style="background-image:url('images/home_instituto_002.jpg');"></div>
+			</div>
+			<div class="item">
+				<div class="fill" style="background-image:url('images/home_instituto_003.jpg');"></div>
+			</div>
+		</div>
 
-      <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="arrow arrow-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="arrow arrow-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-		</section>
-		<!-- ./Slider -->
-		<!-- section -->
-		<div class="section">
-			<div class="container">
+		<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+			<span class="arrow arrow-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+			<span class="arrow arrow-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</section>
+<!-- ./Slider -->
+<!-- section -->
+<div class="section especialties">
+	<div class="container">
 
-				<div class="section-heading dark">
-					<h2>Our Services</h2>
-				</div>
+		<div class="section-heading dark">
+			<h2>Especialidades</h2>
+		</div>
 
-				<div class="row">
-					<div class="col-md-4 col-sm-4">
-						<div class="icon-box text-center wow fadeInLeft showdelay2">
-							<img src="images/icon1.png" alt="icon1" />
-							<h3>Android Image</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+		<div class="row">
+			<div class="col-md-4 col-sm-4">
+				<div class="icon-box text-center wow fadeInLeft showdelay2">
+					<img src="images/espec_001_medicina_integrativa.png" height="97" width="96" alt="icon1" />
+					<h3>Medicina Integrativa</h3>
+					<p>O conceito de Medicina Integrativa Ambiental se fundamenta no conhecimento do ambiente em que vivemos e dos impactos que os fatores do meio
+						ambiente externo, e também do meio interno, interferem na saúde do indivíduo. Ou seja, é a prática da Medicina que reafirma a importância da
+						relação entre o paciente e o profissional de saúde, focada na pessoa em seu todo, baseada em evidências, utilizando todas as abordagens
+						terapêuticas e disciplinas adequadas para obter o melhor da saúde e cura.</p>
 
-						</div>
 					</div>
-					<div class="col-md-4 col-sm-4">
-						<div class="icon-box text-center wow fadeInUp showdelay2">
-							<img src="images/icon2.png" alt="icon2" />
-							<h3>Apple Image</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="icon-box text-center wow fadeInUp showdelay2">
+						<img src="images/espec_002_odontologia_biologica.png" height="97" width="96" alt="icon2" />
+						<h3>Odontologia Biológica</h3>
+						<p>Com base na noção de saúde integral, a odontologia biológica distingue­-se por
+							respeitar a individualidade, ponderar o efeito que os procedimentos e
+							materiais odontológicos têm sobre o organismo, e fazer uso de técnicas
+							menos invasivas, menos traumáticas e menos tóxicas, tanto para o
+							diagnóstico, como para o tratamento dos dentes e arquitetura da face.</p>
 
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-4">
 						<div class="icon-box text-center wow fadeInRight showdelay2">
-							<img src="images/icon3.png" alt="icon3" />
-							<h3>RSS Image</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+							<img src="images/espec_003_terapias.png" height="97" width="96" alt="icon3" />
+							<h3>Terapias</h3>
+							<p>Além de empregar o recurso terapêutico ideal para cada caso, a combinação de técnicas já consagradas da abordagem convencional com
+                diferentes modalidades de terapias complementares, visam dar suporte para que o próprio indivíduo contribua para o seu processo de cura,
+                pois estimulam a capacidade natural de auto­ regulação que o organismo humano possui.</p>
 						</div>
 					</div>
 				</div>
@@ -69,23 +78,28 @@
 				<div class="row">
 					<div class="col-md-4 col-sm-4">
 						<div class="icon-box text-center wow fadeInLeft showdelay2">
-							<i class="fa fa-android"></i>
-							<h3>Android Icon</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+							<img src="images/espec_004_nutricao_funcional.png" height="97" width="96" alt="icon4">
+							<h3>Nutrição Funcional</h3>
+							<p>Através do equilíbrio e da alimentação correta, é possível prevenir doenças e manter o equilíbrio físico e emocional, promovendo a saúde.
+                Além das questões relativas à obesidade, a nutrição é fator determinante para a estética. Nossos especialistas atuam de acordo com as
+                expectativas do paciente e das metas que ele queira atingir.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-4">
 						<div class="icon-box text-center wow fadeInUp showdelay2">
-							<i class="fa fa-apple"></i>
-							<h3>Apple Icon</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+						<img src="images/espec_005_estetica.png" height="97" width="96" alt="icon5">
+							<h3>Estética</h3>
+							<p>Oferecemos diversos tratamentos estéticos não invasivos, buscando atender as suas necessidades de forma saudável e orgânica.
+                Contamos com profissionais altamente capacitados e especializados que trabalham com os mais modernos e exclusivos tipos de tratamento.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-4">
 						<div class="icon-box text-center wow fadeInRight showdelay2">
-							<i class="fa fa-rocket"></i>
-							<h3>Rocket Icon</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit Quisque rutrum pellentesque imperdiet.</p>
+						<img src="images/espec_006_exames.png" height="97" width="96" alt="icon6">
+							<h3>Exames</h3>
+							<p>Seguindo o conceito de respeito à integridade do biossistema humano, oferecemos exames que priorizam a forma não invasiva, diagnosticando com precisão as alterações e necessidades do corpo.
+                Sinais de equilíbrio e possíveis desequilíbrios que o corpo possa vir a ter, são apresentados ao paciente que, após análise pela equipe do Alpha Group,
+                será direcionado para o tratamento mais indicado.</p>
 						</div>
 					</div>
 				</div>
@@ -430,7 +444,5 @@
 		<script src="js/jqbootstrapvalidation/jqBootstrapValidation.js"></script>
 		<!-- main -->
 		<script src="js/main.js"></script>
-
-
 	</body>
 	</html>
