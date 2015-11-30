e11b288e5b73dec392ce8702840e3ccc3b84b42d
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-11-29 23:24:10 -0200

    atualizando font awesomome and home page with effects

---
+++ _footer.php
@@ -0,0 +1,94 @@
+            <footer class="footer section">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
+                      <h3>Fique ligado!</h3>
+                    <p>Fique ligado nas dicas e novidades sobre saúde através da nossa fanpage e no Blog da Emília!</p>
+                    <ul class="list-unstyled">
+                      <li><a href="#"><strong>Blog da Emília</strong></a></li>
+                      <li><a href="https://www.facebook.com/pages/Instituto-Alpha-de-Sa%C3%BAde-Integral/385416424931241"><strong>Facebook/</strong>AlphaGroup</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
+                      <h3>Mapa do site</h3>
+                    <ul class="list-unstyled">
+                      <li><a href="index.php">Home</a></li>
+                      <li><a href="alphagroup.php">O Alpha Group</a></li>
+                      <li><a href="galeria.php">Galeria</a></li>
+                      <li><a href="profissonais.php">Os profissionais</a></li>
+                      <li><a href="parceiros.php">Parceiros</a></li>
+                      <li><a href="contato.php">Contato</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
+                    <h3>Redes Sociais</h3>
+                    <div class="social">
+                      <ul class="social-link">
+                        <li><a href="https://www.facebook.com/pages/Instituto-Alpha-de-Sa%C3%BAde-Integral/385416424931241" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/InstitutoAlpha" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.linkedin.com/company/instituto-alpha-de-sa%C3%BAde-integral" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- address -->
+                  <div class="col-md-3 col-sm-3">
+                    <div class="address  wow fadeInUp showdelay2">
+                      <h3>Endereços</h3>
+                      <ul class="unstyled">
+                        <li><i class="fa fa-map-marker"></i>Av. Rebouças, 3377, Pinheiros, São Paulo - SP</li>
+                        <li><i class="fa fa-envelope"></i>contato@alphagroup.com.br</li>
+                        <li><i class="fa fa-phone"></i>+55 (11) 3078-6777</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- ./address -->
+
+
+                </div>
+                <!-- ./row -->
+              </div>
+
+            </footer>
+            <!-- ./footer -->
+
+            <!-- bottom-footer -->
+            <div class="bottom-footer">
+              <div class="container">
+                <div class="text-center">
+                  &copy; 2015 GZ Studio | <a href="http://www.gzstudio.com.br">GZ Studio </a>
+                </div>
+              </div>
+            </div>
+            <!-- ./bottom-footer -->
+
+          </div>
+          <!-- ./wrapper -->
+
+
+          <!-- jQuery  -->
+          <script src="js/jquery.js"></script>
+          <!-- bootstrap -->
+          <script src="js/bootstrap.min.js"></script>
+          <!-- easing -->
+          <script src="js/easing/jquery.easing.1.3.js"></script>
+          <!-- wow -->
+          <script src="js/wow/wow.min.js"></script>
+          <!-- backstretch -->
+          <script src="js/backstretch/jquery.backstretch.min.js"></script>
+          <!-- owl carousel -->
+          <script src="js/owl-carousel/owl.carousel.js"></script>
+          <!-- Magnific Popup -->
+          <script src="js/magnific-popup/jquery.magnific-popup.js"></script>
+          <!-- waypoints -->
+          <script src="js/waypoints/waypoints.js"></script>
+          <!-- counterup -->
+          <script src="js/counterup/jquery.counterup.js"></script>
+          <!-- countdown -->
+          <script src="js/countdown/jquery.countdown.min.js"></script>
+          <!-- UIToTop -->
+          <script src="js/uitotop/jquery.ui.totop.min.js"></script>
+          <!-- jqbootstrapvalidator -->
+          <script src="js/jqbootstrapvalidation/jqBootstrapValidation.js"></script>
+          <!-- main -->
+          <script src="js/main.js"></script>
--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 
 		<div class="carousel-inner" role="listbox">
 			<div class="item active">
-				<div class="fill" style="background-image:url('images/home_instituto_001.jpg');"></div>
+				<div class="fill" style="background-image:url('images/home_instituto_001.jpg'); height: 650px;"></div>
 			</div>
 			<div class="item">
-				<div class="fill" style="background-image:url('images/home_instituto_002.jpg');"></div>
+				<div class="fill" style="background-image:url('images/home_instituto_002.jpg'); height: 650px;"></div>
 			</div>
 			<div class="item">
-				<div class="fill" style="background-image:url('images/home_instituto_003.jpg');"></div>
+				<div class="fill" style="background-image:url('images/home_instituto_003.jpg'); height: 650px;"></div>
 			</div>
 		</div>
 
@@ -134,233 +134,137 @@
 						<!-- ./section -->
 
 
-							<!-- section -->
-							<div class="section">
-								<div class="container">
+						<!-- section -->
+						<div class="section">
+							<div class="container">
 
-									<div class="section-heading">
-										<h2>Nossa Equipe</h2>
-									</div>
+								<div class="section-heading">
+									<h2>Nossas profissionais</h2>
 
-									<div class="row">
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img1.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img1.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img2.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img2.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img3.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img3.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img4.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img4.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-									</div>
-									<!-- /row -->
-
-									<div class="row">
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img5.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img5.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img6.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img6.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img7.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img7.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-6">
-											<div class="pic wow flipInX showdelay2">
-												<a href="images/img8.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img8.jpg" alt=""><div class="img_overlay"></div></a>
-											</div>
-										</div>
-									</div>
-									<!-- /row -->
-
+									<p class="text-center">
+										Atendendo as necessidades da vida atual, o Instituto Alpha oferece uma estrutura completa de profissionais, equipamentos, diagnósticos e exames
+										para a otimização do seu tempo e da sua visita.<br>
+										Conheça cada um de nossa equipe através das fotos.
+									</p>
 								</div>
+
+								<div class="row">
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img1.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img1.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img2.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img2.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img3.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img3.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img4.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img4.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+								</div>
+								<!-- /row -->
+
+								<div class="row">
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img5.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img5.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img6.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img6.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img7.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img7.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-6">
+										<div class="pic wow flipInX showdelay2">
+											<a href="images/img8.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/img8.jpg" alt=""><div class="img_overlay"></div></a>
+										</div>
+									</div>
+								</div>
+								<!-- /row -->
 
 							</div>
-							<!-- ./section -->
-
-							<!-- section -->
-							<div class="section">
-								<div class="container">
-
-									<div class="section-heading">
-										<h2>Parceiros de confiança</h2>
-									</div>
-									<!-- latest-news -->
-									<div class="row wow bounceInDown">
-										<div class="col-md-3 col-sm-3">
-											<div class="featured-box item text-center">
-												<img class="img-responsive img-thumbnail" src="images/img5.jpg" alt="">
-												<div class="featured-info">
-													<h3>Transportation</h3>
-													<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
-													<a href="#" class="btn btn-info">Read More</a>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-3">
-											<div class="featured-box item text-center">
-												<img class="img-responsive img-thumbnail" src="images/img6.jpg" alt="">
-												<div class="featured-info">
-													<h3>Transportation</h3>
-													<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
-													<a href="#" class="btn btn-info">Read More</a>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-3">
-											<div class="featured-box item text-center">
-												<img class="img-responsive img-thumbnail" src="images/img7.jpg" alt="">
-												<div class="featured-info">
-													<h3>Transportation</h3>
-													<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
-													<a href="#" class="btn btn-info">Read More</a>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-3 col-sm-3">
-											<div class="featured-box item text-center">
-												<img class="img-responsive img-thumbnail" src="images/img8.jpg" alt="">
-												<div class="featured-info">
-													<h3>Transportation</h3>
-													<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
-													<a href="#" class="btn btn-info">Read More</a>
-												</div>
-											</div>
-										</div>
-									</div>
-									<!-- ./latest-news -->
-
-								</div>
-
-							</div>
-							<!-- ./section -->
-
-							<!-- section -->
-							<div class="section bg-gray">
-								<div class="container">
-
-									<div class="row">
-										<div class="col-md-9">
-											<h3>This is a Callout Section</h3>
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ultrices ligula. Curabitur consectetur lobortis velit,</p>
-										</div>
-										<div class="col-md-3">
-											<a href="#" class="btn btn-info btn-lg btn-callout">Buy me Now!</a>
-										</div>
-									</div>
-
-								</div>
-							</div>
-							<!-- ./section -->
-
-							<!-- footer -->
-							<footer class="footer section">
-								<div class="container">
-									<div class="row">
-										<div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
-											<h3>About Us</h3>
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ultrices ligula. Curabitur consectetur lobortis velit, eget semper quam fringilla ut. </p>
-											<button type="button" class="btn btn-sm btn-custom">Readmore.. </button>
-										</div>
-										<div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
-											<h3>Pages Links</h3>
-											<ul class="arrow">
-												<li><a href="#">Home</a></li>
-												<li><a href="#">About</a></li>
-												<li><a href="#">FAQ</a></li>
-												<li><a href="#">Career</a></li>
-												<li><a href="#">Contact </a></li>
-											</ul>
-										</div>
-										<div class="col-md-3 col-sm-3 wow fadeInUp showdelay2">
-											<h3>Be Social</h3>
-											<div class="social">
-												<ul class="social-link">
-													<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-													<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-													<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-													<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-												</ul>
-											</div>
-										</div>
-										<!-- address -->
-										<div class="col-md-3 col-sm-3">
-											<div class="address  wow fadeInUp showdelay2">
-												<h3>Address</h3>
-												<ul class="unstyled">
-													<li><i class="fa fa-map-marker"></i>25th Street, Suite 2 New York NY 10887</li>
-													<li><i class="fa fa-envelope"></i>email@domain.com</li>
-													<li><i class="fa fa-phone"></i>+40 (0) 231 1234 567</li>
-													<li><i class="fa fa-fax"></i>+40 (0) 123 1234 568</li>
-												</ul>
-											</div>
-										</div>
-										<!-- ./address -->
-
-
-									</div>
-									<!-- ./row -->
-								</div>
-
-							</footer>
-							<!-- ./footer -->
-
-							<!-- bottom-footer -->
-							<div class="bottom-footer">
-								<div class="container">
-									<div class="text-center">
-										&copy; 2015 GZ Studio | <a href="http://www.gzstudio.com.br">GZ Studio </a>
-									</div>
-								</div>
-							</div>
-							<!-- ./bottom-footer -->
 
 						</div>
-						<!-- ./wrapper -->
+						<!-- ./section -->
 
+						<!-- section -->
+						<div class="section partners">
+							<div class="container">
 
-						<!-- jQuery  -->
-						<script src="js/jquery.js"></script>
-						<!-- bootstrap -->
-						<script src="js/bootstrap.min.js"></script>
-						<!-- easing -->
-						<script src="js/easing/jquery.easing.1.3.js"></script>
-						<!-- wow -->
-						<script src="js/wow/wow.min.js"></script>
-						<!-- backstretch -->
-						<script src="js/backstretch/jquery.backstretch.min.js"></script>
-						<!-- owl carousel -->
-						<script src="js/owl-carousel/owl.carousel.js"></script>
-						<!-- Magnific Popup -->
-						<script src="js/magnific-popup/jquery.magnific-popup.js"></script>
-						<!-- waypoints -->
-						<script src="js/waypoints/waypoints.js"></script>
-						<!-- counterup -->
-						<script src="js/counterup/jquery.counterup.js"></script>
-						<!-- countdown -->
-						<script src="js/countdown/jquery.countdown.min.js"></script>
-						<!-- UIToTop -->
-						<script src="js/uitotop/jquery.ui.totop.min.js"></script>
-						<!-- jqbootstrapvalidator -->
-						<script src="js/jqbootstrapvalidation/jqBootstrapValidation.js"></script>
-						<!-- main -->
-						<script src="js/main.js"></script>
-					</body>
-					</html>
+								<div class="section-heading">
+									<h2>Parceiros de confiança</h2>
+									<p>
+										O Alpha Group, em função da competência de seus profissionais e serviços, estabeleceu algumas importantes parcerias técnico-científicas.
+									</p>
+								</div>
+								<!-- latest-news -->
+								<div class="row wow bounceInDown">
+									<div class="col-md-3 col-sm-3">
+										<div class="featured-box item text-center">
+											<img class="img-responsive img-thumbnail" src="images/img5.jpg" alt="">
+											<div class="featured-info">
+												<h3>Transportation</h3>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
+												<a href="#" class="btn btn-info">Read More</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-3">
+										<div class="featured-box item text-center">
+											<img class="img-responsive img-thumbnail" src="images/img6.jpg" alt="">
+											<div class="featured-info">
+												<h3>Transportation</h3>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
+												<a href="#" class="btn btn-info">Read More</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-3">
+										<div class="featured-box item text-center">
+											<img class="img-responsive img-thumbnail" src="images/img7.jpg" alt="">
+											<div class="featured-info">
+												<h3>Transportation</h3>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
+												<a href="#" class="btn btn-info">Read More</a>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-3 col-sm-3">
+										<div class="featured-box item text-center">
+											<img class="img-responsive img-thumbnail" src="images/img8.jpg" alt="">
+											<div class="featured-info">
+												<h3>Transportation</h3>
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit.</p>
+												<a href="#" class="btn btn-info">Read More</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- ./latest-news -->
+
+							</div>
+
+						</div>
+						<!-- ./section -->
+
+						<!-- section -->
+
+						<!-- footer -->
+<?php include "_footer.php";?>
+
+				</body>
+				</html>
