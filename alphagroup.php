03cce5e6598a905732ebfe9437064534383e8dfa
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-12-01 08:57:00 -0200

    site alpha com efeitos

--- alphagroup.php
+++ alphagroup.php
@@ -20,7 +20,7 @@
 			<div class="col-md-6 col-sm-6">
 				<h2>Sobre o Alpha Group</h2>
 				<p class="text-justify">
-				Fundado em 2003, o Alpha Group tornou-se muito mais do que uma clínica médica
+					Fundado em 2003, o Alpha Group tornou-se muito mais do que uma clínica médica
 					multidisciplinar especializada, pois no mesmo espaço físico, estão reunidos os mais
 					conceituados médicos e profissionais da área da saúde, objetivando a integração do indivíduo
 					como um todo, além de métodos de diagnóstico e de tratamento, convencionais e inovadores,
@@ -30,7 +30,7 @@
 			</div>
 
 			<div class="col-md-6 col-sm-6">
-					<img src="images/alphagroup_hearts.jpg" height="477" width="448" alt="ladder">
+				<img src="images/alphagroup_hearts.jpg" height="477" width="448" alt="ladder">
 			</div>
 
 		</div>
@@ -38,14 +38,73 @@
 
 	</div>
 </div>
-<!-- ./section -->
 
-<!-- section -->
+<div class="section">
+	<div class="container">
+		<p class="grey-text text-intern">
+			Saúde se conquista diariamente, eliminando o que não serve mais ou que é tóxico, e aportando novas informações de qualidade, nutrientes e pensamentos com igual importância.
+		</p>
+		<h3 class="text-center">
+			Nós somos o Alpha Group! Nossa maior especialidade é a sua saúde!
+		</h3>
+	</div>
+	<div class="container">
+		<h1 class="green-text">Missão</h1>
+		<p class="text-intern grey-text text-justify">
+			Promover a prevenção consistente, a recuperação da saúde e o bem estar global do indivíduo, envolvendo-o em seu processo de prevenção e cura, <br>
+			harmonizando as estruturas física, energética, emocional e mental, por meio de tecnologias de ponta e atuação de
+			uma equipe multidisciplinar e humanizada.
+		</p>
+		<h1 class="green-text">Visão</h1>
+		<p class="text-intern grey-text text-justify">
+			Tornar-se um centro de Medicina Integrativa Ambiental de referência, reconhecido nacional e
+			mundialmente, promovendo saúde de forma humanizada e personalizada, visando o bem estar
+			e qualidade de vida.
+		</p>
+		<h1 class="green-text">Valores</h1>
+		<p class="text-intern grey-text text-justify">
+			Reunir e difundir as melhores práticas a favor da vida, atuando com segurança,
+			qualidade e inovação. Valorizando sempre a confiabilidade e transparência dos
+			processos, o respeito à todos os indivíduos, e principalmente, o respeito à vida.
+		</p>
+	</div>
 
-<!-- ./section -->
+	<div class="alphagroup-bg">
+		<div class="container">
+			<h1 class="white-text">Sobre a Dra Em&iacute;lia Gadelha</h1>
+			<br><Br>
+			<p class="white-text">
+				Formada pela Universidade Federal do Rio de Janeiro (UFRJ) em 1988, residência médica em Otorrinolaringologia pela Universidade <br>
+				de São Paulo - campus Ribeirão Preto, pós-graduação (Mestrado) em Otorrinolaringologia e Cirurgia de Cabeça e Pescoço pela <br>
+				Escola Paulista de Medicina (atual UNIFESP).<br>
+				<Br>
+					Bolsista do MONBUSHO (Ministério da Educação, Ciência e Cultura do Governo Japonês) na área de laringe e voz.<br>
+					<br>
+					Formação em Medicina Biológica Alemã, é membro da Diretoria da Associação Brasileira de Homeopatia e Homotoxicologia (ABHH).<br>
+					<br>
+					Foi a 1&ordf;. Diretora Presidente da Associação Brasileira de Ozonioterapia (ABOZ) e atualmente é Diretora Secretária da ABOZ. <br>
+					<br>
+					Ministra aulas para cursos de extensão e pós-graduação em todo o Brasil sobre “Microecologia Intestinal e Adoecimento Sistêmico” e “Ozonioterapia”.<br>
+					<br>
+					Foi Diretora-Presidente e fundadora da organização não-governamental Instituto Recicle Milhões de Vidas (atual Instituto Recicle), dedicada a promover<br>
+					educação ambiental e gerar recursos financeiros para outras instituições que atendem crianças carentes a partir da implantação de programas de coleta <br>
+					seletiva de lixo.
+					<br>
+					Formação complementar: cursos nas áreas: Medicina Hiperbárica, Homeopatia, Psicossomática, Medicina Tradicional Chinesa e Acupuntura, <br>
+					Homotoxicologia, Ozonioterapia, Oligoterapia Catalítica, Auriculoacupuntura, Bioressonância,  Prática Ortomolecular, Microscopia, Iridologia, Eneagrama, <br>
+					Resíduos Sólidos (lixo) e Preservação do Meio Ambiente.<br>
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- ./section -->
 
-<!-- ./section -->
-<?php include "_footer.php";?>
-<!-- footer -->
+	<!-- section -->
+
+	<!-- ./section -->
+
+	<!-- ./section -->
+	<?php include "_footer.php";?>
+	<!-- footer -->
 </body>
 </html>
