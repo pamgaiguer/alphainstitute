3cf8746d837160d625b6b723fa88ee70af58af9d
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2015-12-21 01:33:02 -0200

    partners

--- partners.php
+++ partners.php
@@ -1,67 +1,169 @@
 <?php include "_header.php";?>
 <!-- ./navigation -->
-			<!-- ./navigation -->
-			<!-- section -->
-			<div class="section page-header">
-				<div class="container">
-					<h1>Nossos Parceiros</h1>
+<!-- ./navigation -->
+<!-- section -->
+<div class="section page-header">
+	<div class="container">
+		<h1>Nossos Parceiros</h1>
+	</div>
+</div>
+<!-- ./section -->
+<!-- section -->
+<div class="section">
+	<div class="container container-partners">
+
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo1-paracelsus.jpg" alt="paracelsus">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Paracelsus Klinik</h4>
+						<p class="text-justify">
+							O Paracelsus Klinik Lustm&uuml;hle é a clínica mais antiga de Medicina Biológica do mundo, localizada em Lustm&uuml;hle, Suiça.
+							Por meio desta associação, nossos pacientes dispõem de uma segunda opinião com o Dr. Thomas Rau, o mundialmente conhecido
+							Diretor Médico da Paracelsus Klinik, bem como de tratamento personalizado com medicamentos importados que se façam necessários.
+						</p>
+						<a href="http://www.paracelsus.ch" target="_blank">http://www.paracelsus.ch</a>
+					</div>
 				</div>
 			</div>
-			<!-- ./section -->
-			<!-- section -->
-			<div class="section">
-				<div class="container">
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo2-katia.jpg"  alt="katia">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Clínica Dra. Kátia Volpe</h4>
+						<p class="text-justify">
+							A Clínica Dra. Kátia Volpe oferece tratamentos não-cirúrgicos na área da Dermatologia Cosmética e Laser, com segurança comprovada
+							por diversos estudos científicos em revistas indexadas e que proporcionam resultados naturais.
+						</p>
+						<a href="http://www.clinicakatiavolpe.com.br" target="_blank">http://www.clinicakatiavolpe.com.br</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo3-institutomachado.jpg" alt="machado">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Instituto Machado</h4>
+						<p class="text-justify">
+							O Instituto Machado possui todas as especialidades da odontologia e com uma base filosófica acadêmica, está sempre em busca da excelência em todos os
+							trabalhos, e para isto sua equipe é constituída por profissionais obrigatoriamente especialistas atuantes em suas respectivas áreas.
+						</p>
+						<a href="http://www.institutomachado.com.br" target="_blank">http://www.institutomachado.com.br</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo_sorrentino.jpg" alt="victor">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Clínica Dr. Victor Sorrentino</h4>
+						<p class="text-justify">
+							O primeiro Centro Premium de Medicina Preventiva do Brasil. Com serviços que modelam-se aos interesses dos mais exigentes pacientes e agregam os
+							resultados impressionantes da Ciência Medica conhecida como a <strong>"The Future Medicine"</strong>.<br>
+							Proporcionando o equilíbrio entre a Atividade-Física regular e assistida, a Administração das Emoções, a Otimização Hormonal, a Nutrigenetica e a
+							Manutenção do Estresse são a base da Medicina Preventiva praticada de forma exemplar e pioneira no Sorrentino Longevity Center.
+						</p>
+						<a href="http://www.drvictorsorrentino.com.br" target="_blank">http://www.drvictorsorrentino.com.br</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo_alta.jpg" alt="alta">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Middle aligned media</h4>
+						<p class="text-justify">
+								O Alta Excelência Diagnóstica une três pilares: equipe médica de renome internacional, equipamentos de última geração e excelência em serviços. <br>
+								Além de diagnóstico por imagem, a marca oferece mais de três mil tipos de exames de análises clínicas que contam com a precisão de equipamentos de última geração, como
+								também medicina personalizada, genética, biologia molecular, entre outros.
+						</p>
+						<a href="http://www.altadiagnosticos.com.br" target="_blank">http://www.altadiagnosticos.com.br</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo_microecologia.jpg" alt="microecologia">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Instituto Microecologia</h4>
+						<p class="text-justify">
+							O Instituto de Microecologia, fundado na Alemanha há mais de 50 anos, atua na área de Microbiologia aplicada à clínica médica.
+							Responsável pela criação do conceito de micro-ecologia do organismo, observando a relação entre a microflora bacteriana e a saúde humana, desenvolve métodos de
+							análise e quantificação da microflora humana, correlacionando-os com as diversas situações patológicas.
+						</p>
+						<a href="http://www.microecologia.com.br" target="_blank">http://www.microecologia.com.br</a>
 
-				<div class="row">
-					<div class="col-md-3 col-sm-3">
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="media">
+					<div class="media-left media-middle">
+						<a href="#">
+							<img class="thumbnail media-object" src="images/logos_parceiros/logo_renatotatagiba.jpg" alt="renatotatagiba">
+						</a>
+					</div>
+					<div class="media-body">
+						<h4 class="media-heading">Clínica Renato Tatagiba Cirurgia Plástica</h4>
+						<p class="text-justify">
+							A Clínica Renato Tatagiba Cirurgia Plástica é muito bem estruturada com equipamentos ultramodernos que proporcionam aos pacientes segurança e tranquilidade,
+							além de anos de experiência que transmitem credibilidade e um atendimento diferenciado para realização das cirurgias.
+						</p>
+						<a href="http://www.renatotatagiba.com.br" target="_blank">http://www.renatotatagiba.com.br</a>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<!-- /row -->
+
+	</div>
+</div>
+<!-- ./section -->
+
+<!-- 					<div class="col-md-3 col-sm-3">
 						<div class="pic wow flipInX showdelay2">
 							<a href="images/logos_parceiros/logo1-paracelsus.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo1-paracelsus.jpg" alt="paracelsus"><div class="img_overlay"></div></a>
 						</div>
-					</div>
-					<div class="col-md-3 col-sm-3">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo2-katia.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo2-katia.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-					<div class="col-md-3 col-sm-3">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo3-institutomachado.jpg"  class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo3-institutomachado.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-					<div class="col-md-3 col-sm-3">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo_sorrentino.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo_sorrentino.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-				</div>
-				<!-- /row -->
+					</div> -->
 
-				<div class="row">
-					<div class="col-md-4">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo_alta.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo_alta.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo_microecologia.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo_microecologia.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="pic wow flipInX showdelay2">
-							<a href="images/logos_parceiros/logo_renatotatagiba.jpg" class="gallery-item"><img class="img-responsive img-thumbnail" src="images/logos_parceiros/logo_renatotatagiba.jpg" alt=""><div class="img_overlay"></div></a>
-						</div>
-					</div>
-				</div>
-				<!-- /row -->
-
-				</div>
-			</div>
-			<!-- ./section -->
-
-
-	<!-- ./section -->
-	<?php include "_footer.php";?>
-	<!-- footer -->
-</body>
-</html>
+					<!-- ./section -->
+					<?php include "_footer.php";?>
+					<!-- footer -->
+				</body>
+				</html>
