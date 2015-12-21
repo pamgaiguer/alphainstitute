29ca930f258134f13f28774f7ff1280053cb7fd7
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2015-12-20 22:57:28 -0200

    modal centralizado - preparando loop for com ajax dos modais

--- professionals.php
+++ professionals.php
@@ -9,10 +9,61 @@
 <!-- section -->
 <div class="section">
 	<div class="container">
-		<div class="container-med">
-		<!-- conteúdo montado pelo javascript !  -->
-		</div>
-	</div>
+
+<a href="#" data-toggle="modal" data-target="#myModal">
+
+    <div class='col-md-6 col-sm-6 wow fadeInUp showdelay2'>
+      <div class='media team-box'>
+        <div class='media-bottom team-image'>
+        <img class='media-object img-responsive' src="images/professionals_internas/alexandre_thumb.png" />
+        </div>
+        <div class='team-info'>
+          <h3>nomecompleto</h3>
+          <strong>profissão</strong>
+          <p class='text-justify'>slogan</p>
+        </div>
+      </div>
+    </div>
+</a>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content" id="modal_alexandre">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span>&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <div class='col-md-6 col-sm-6 wow fadeInUp showdelay2'>
+      <div class='media team-box'>
+        <div class='media-bottom team-image'>
+        <img class='media-object img-responsive' src="images/professionals_internas/alexandre_thumb.png" />
+        </div>
+        <div class='team-info'>
+          <h3>nomecompleto</h3>
+          <strong>profissão</strong>
+          <p class='text-justify'>slogan</p>
+        </div>
+      </div>
+    </div>
+
+
+
+    <hr>
+    <div class="container-med">
+      <!-- conteúdo montado pelo javascript !  -->
+    </div>
+  </div>
 </div>
 
 <!-- section -->
